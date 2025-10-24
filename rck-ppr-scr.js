03a594356eef7f57c0a5eb181587b5a8c6602710
dizzydrwaf <dizzydrwaf@gmail.com>
2025-10-24 13:43:50 +0200

    idc

---
+++ rck-ppr-scr.js
@@ -0,0 +1,9 @@
+let rock_btn
+rock_btn = document.getElementById("rock")
+let paper_btn
+paper_btn = document.getElementById("paper")
+let scissors_btn 
+scissors_btn = document.getElementById("scissors")
+
+scissors_btn.addEventListener("click", alert("hello"))
+rock_btn.addEventListener("click", alert("hello!"))
